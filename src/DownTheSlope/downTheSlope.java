584a52cd551ad63780c049f3943110100fea5174
javirola <j.rodrigeu>
2021-09-29 11:28:18 +0200

    initial commit

---
+++ src/DownTheSlope/downTheSlope.java
@@ -0,0 +1,9 @@
+package DownTheSlope;
+
+public class downTheSlope {
+    public static int downTheSlope(char[][] slopeMap, int right, int down ){
+       char slopeMap[11][11] = {{".", ".", "#", ".", ".",".", ".", ".", ".", "." }
+                                {"#", ".", ".", ".", "#",""}
+       }
+    }
+}
